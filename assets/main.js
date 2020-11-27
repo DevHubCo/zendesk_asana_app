e30cecb922b5110716c83ed0d0b0d348d6890af3
ysj151215 <jailjt.editor@gmail.com>
2020-11-27 15:45:26 +0800

    get zendesk subject’s name & url

--- assets/main.js
+++ assets/main.js
@@ -1,51 +1,66 @@
-$(function() {
-    var client = ZAFClient.init();
-    client.invoke('resize', { width: '100%', height: '400px' });
-    showForm();
-  
-    $("#add-btn").click(function(event) {
-      event.preventDefault();
-      if ($("#name").val().length == 0) {
-        client.invoke('notify', 'Name can\'t be blank.', 'error');
-      } else {
-        var task = {
-          data: {
-            name: $("#name").val(),
-            notes: $("#notes").val(),
-            projects: $("#project-id").val(),
-          }
-        };
-        console.log(task)
-        sendTaskData(task, client);
-      }
-    });
-  
-  });
-  
-function showForm() {
-    var source = $("#add_task-hdbs").html();
-    var template = Handlebars.compile(source);
-    var html = template();
-    $("#content").html(html);
+const ASANA_API = 'https://app.asana.com/api/1.0/tasks'
+const ASANA_AUTHORIZATION = 'Bearer 1/1192875441845806:83153e157aedc4314451ee5dcf6bd2a2'
+
+$(function () {
+	const client = ZAFClient.init()
+	client.invoke('resize', { width: '100%', height: '250px' })
+	showForm(client)
+
+	$('#add-btn').click(function (e) {
+		e.preventDefault()
+
+		if ($('#task_name').val().length === 0) {
+			client.invoke('notify', "Name can't be blank.", 'error')
+		} else {
+			const task = {
+				data: {
+					task_name: $('#task_name').val(),
+					task_url: $('#task_url').val(),
+					projects: $('#project-id').val()
+				}
+			}
+			console.log(task)
+			sendTaskData(client, task)
+		}
+	})
+})
+
+function sendTaskData(client, task) {
+	const settings = {
+		url: ASANA_API,
+		type: 'POST',
+		contentType: 'application/json',
+		headers: { Authorization: ASANA_AUTHORIZATION },
+		data: JSON.stringify(task)
+	}
+
+	client.request(settings).then(
+		function () {
+			client.invoke('notify', 'Task successfully added to Asana.')
+			$('#task-form')[0].reset()
+		},
+		function (err) {
+			const msg = `Error${err.status} ${err.statusText}`
+			client.invoke('notify', msg, 'error')
+		}
+	)
+
+	client.invoke('notify', 'Task sent! Please wait...')
 }
-  
-function sendTaskData(task, client) {
-    var settings = {
-      url: 'https://app.asana.com/api/1.0/tasks',
-      headers: {"Authorization": "Bearer 1/1192875441845806:83153e157aedc4314451ee5dcf6bd2a2"},
-      type: 'POST',
-      contentType: 'application/json',
-      data: JSON.stringify(task)
-    };
-    client.request(settings).then(
-      function() {
-        client.invoke('notify', 'Task successfully added to Asana.');
-        $('#task-form')[0].reset();
-      },
-      function(response) {
-        var msg = 'Error ' + response.status + ' ' + response.statusText;
-        client.invoke('notify', msg, 'error');
-      }
-    );
-    client.invoke('notify', 'Task sent! Please wait...');
+
+function showForm(client) {
+	let requester_data = {
+		task_name: '',
+		task_url: ''
+	}
+
+	client.get('ticket').then(function (data) {
+		requester_data.task_name = data.ticket.subject
+		requester_data.task_url = `https://devhubco.zendesk.com/agent/tickets/${data.ticket.id}`
+
+		const source = $('#add-task-template').html(),
+			template = Handlebars.compile(source),
+			html = template(requester_data)
+		$('#content').html(html)
+	})
 }
